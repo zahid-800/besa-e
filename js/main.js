cc0c8da50041e3f8c3fe2841b0c8590f55a829e5
zahid-800 <154730312+zahid-800@users.noreply.github.com>
2024-02-14 00:10:58 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,94 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  function navBar(){
+    const navMobile=document.getElementById('mobileShow');
+    navMobile.style.display='flex';
+  }
+
+  function navBarcross(){
+    const navMobile=document.getElementById('mobileShow');
+    navMobile.style.display='none';
+  }
+
+
+
+
+
+// You can also use <link> for styles animation
+// ..
+AOS.init({
+  duration:1000,
+});
+
+        // Trending on Besa
+       
+        // slider
+       
+        // Editor's Picks
+        function eproductChange(){
+          const eproduct= document.getElementById('imgchange');
+          eproduct.src='./img/fc1-1.webp';
+        }
+        function eproductChange1(){
+          const eproduct= document.getElementById('imgchange');
+          eproduct.src='./img/e1-2.webp';
+        }
+        function eproductChange2(){
+          const eproduct= document.getElementById('imgchange');
+          eproduct.src='./img/fc1-3.webp';
+        }
+      
+        function eproductChange3(){
+          const eproduct= document.getElementById('imgchange2');
+          eproduct.src='./img/e2-1.webp';
+        }
+        function eproductChange4(){
+          const eproduct= document.getElementById('imgchange2');
+          eproduct.src='./img/e2-2.webp';
+        }
+        function eproductChange5(){
+          const eproduct= document.getElementById('imgchange2');
+          eproduct.src='./img/e2-3.webp';
+        }
+      
+        function eproductChange6(){
+          const eproduct= document.getElementById('imgchange3');
+          eproduct.src='./img/e3-1.webp';
+        }
+        function eproductChange7(){
+          const eproduct= document.getElementById('imgchange3');
+          eproduct.src='./img/e3-2.webp';
+        }
+        function eproductChange8(){
+          const eproduct= document.getElementById('imgchange3');
+          eproduct.src='./img/e3-3.webp';
+        }
+      
+        function eproductChange9(){
+          const eproduct= document.getElementById('imgchange4');
+          eproduct.src='./img/e4-1.webp';
+        }
+        function eproductChange10(){
+          const eproduct= document.getElementById('imgchange4');
+          eproduct.src='./img/e4-2.webp';
+        }
+        function eproductChange11(){
+          const eproduct= document.getElementById('imgchange4');
+          eproduct.src='./img/e4-3.webp';
+        }
+      
+        function editorBtnshow(){
+          document.getElementById('editorbtnshow').style.display='flex';
+        }
+        function editorBtnshow2(){
+          document.getElementById('editorbtnshow2').style.display='flex';
+        }
+        function editorBtnshow3(){
+          document.getElementById('editorbtnshow3').style.display='flex';
+        }
+        function editorBtnshow4(){
+          document.getElementById('editorbtnshow4').style.display='flex';
+        }
+      
